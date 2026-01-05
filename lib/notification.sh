fb33fbb0862d45aa91c158a1d67e53c5e3567776
Frederik S. <71598139+fs1n@users.noreply.github.com>
2026-01-05 18:10:18 +0000

    Fix: Add placeholder implementations for notification functions in notification.sh

--- lib/notification.sh
+++ lib/notification.sh
@@ -1,15 +1,22 @@
+#!/bin/env bash
+# Notification Library for Proxmox VE VirtIO / QEMU GA Updater Script
+
 function notification_send() {
     # Todo: Implement notification logic here
+    :
 }
 
 function notification_email_SMTP() {
     # Todo: Implement email notification logic here
+    :
 }
 
 function notification_email_MSGRAPH() {
     # Todo: Implement Microsoft Graph email notification logic here
+    :
 }
 
 function notification_webhook() {
     # Todo: Implement webhook notification logic here
+    :
 }
